4d06daa5003f2f95026aa313b55e117bd5ee470b
Laura Messa Travin <laura.travin@gmail.com>
2021-04-24 18:21:02 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,8 +3,9 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     # render 'birds/index.html.erb'
     # render plain: "Hello #{@birds[3].name}"
-    # render json: @birds
-    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+    # render json: @birds #instance variable is not necesary any more
+     render json: birds
+#    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
 
 
   end
