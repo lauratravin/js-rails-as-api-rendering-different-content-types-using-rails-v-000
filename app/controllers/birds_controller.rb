f320cd58612f18b1fa1e61fc1ebc09574ccc12d1
Laura Messa Travin <laura.travin@gmail.com>
2021-04-24 18:21:54 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
-  //  @birds = Bird.all
+  #  @birds = Bird.all
     birds = Bird.all
     # render 'birds/index.html.erb'
     # render plain: "Hello #{@birds[3].name}"
