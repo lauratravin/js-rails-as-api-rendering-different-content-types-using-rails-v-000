d9bb404bc6e4126579c02586847094bc51111d61
Laura Messa Travin <laura.travin@gmail.com>
2021-04-24 18:12:55 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,7 +3,9 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     # render 'birds/index.html.erb'
     # render plain: "Hello #{@birds[3].name}"
-    render json: @birds
+    # render json: @birds
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+
 
   end
 end
